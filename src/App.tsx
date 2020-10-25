4e5a166c5fa25e74b4a349be083cabbe73d5948d
Redvan <redvanisation@gmail.com>
2020-10-25 01:11:12 -0400

    Add camera component and necessary dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Webcam from './components/Webcam';
 
 function App() {
   return (
     <>
-      <h1>Hello Hacktoberfest!</h1>
+      {/* <h1>Hello Hacktoberfest!</h1> */}
+      <Webcam />
     </>
   );
 }
