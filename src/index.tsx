2b6a6f02b2f637d63489e6798bdbb632a540d533
Redvan <redvanisation@gmail.com>
2020-10-24 23:07:27 -0400

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './stylesheets/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
