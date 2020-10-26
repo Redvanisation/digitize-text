7a564be8b560e2cf75b0828c984f57cf7a422b2a
willou <35076190+walidbillel@users.noreply.github.com>
2020-10-26 13:18:41 -0400

    third co

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const HomePage = (): JSX.Element => {
-  return <>Home Page!
+  return <> Start By Taking a Picture
     
     <a href="/camera" className="cameraLink"> <button className="cameraBtn">  <i className="fas fa-camera"></i> </button> </a>
   </>
