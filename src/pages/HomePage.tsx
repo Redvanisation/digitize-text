1b5543c867a943eecadb1c365b69dd04f7d5201d
Radouane Khiri <radouanekhiri@Radouanes-MacBook-Pro.local>
2020-12-12 20:33:00 -0500

    Add image upload feature and text

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { css } from 'linaria';
 import { Link } from 'react-router-dom';
 
@@ -19,14 +19,30 @@ const homeStyles = {
 };
 
 const HomePage = (): JSX.Element => {
+    const [textPicture, setTextPicture] = useState<File>();
+
+    const uploadTextPicture = useCallback(
+        (event: React.ChangeEvent<HTMLInputElement>) => {
+            if (event.target.files) {
+                setTextPicture(event.target.files[0]);
+                alert(`${event.target.files[0].name} uploaded successfully!`);
+            }
+        },
+        [setTextPicture]
+    );
+
     return (
-        <div className={homeStyles.root}>
-            Home Page!
-            {/* <img src='' alt='logo' className={homeStyles.logo} /> */}
+        <main className={homeStyles.root}>
+            <h1>Welcome to Digitize Text</h1>
+            <h3>To start using the app please upload a picture with text</h3>
+            <label htmlFor='file-upload'>Upload a picture</label>
+            <input type='file' name='file-upload' onChange={uploadTextPicture} />
+            <h3>or</h3>
+            <h3>Click the camera icon to take a new picture</h3>
             <Link to='/camera' className={homeStyles.cameraLink}>
                 <i className='fas fa-camera'></i>
             </Link>
-        </div>
+        </main>
     );
 };
 
